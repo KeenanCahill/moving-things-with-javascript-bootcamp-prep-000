0b4305450a410b4b9bdcaa739e41c4e7ef294115
Keenan Cahill <thatkeenancahill@gmail.com>
2019-04-30 21:26:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const app = "I don't do much."
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
